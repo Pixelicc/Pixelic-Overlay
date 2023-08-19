1e0531b3122e81856596700b05a2e1b6219b628c
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-08-19 17:06:36 +0200

    Update Blacklist.vue

--- src/views/Settings/Blacklist.vue
+++ src/views/Settings/Blacklist.vue
@@ -17,19 +17,18 @@
                     <v-icon v-if="blacklist.name === 'Personal'">mdi-account</v-icon>
                   </template>
                   <template v-slot:append>
-                    <v-dialog v-model="clearPersonalBlacklistDialog">
-                      <template v-slot:activator="{ props1 }">
-                        <v-tooltip text="Clear Personal Blacklist" v-if="blacklist.name === 'Personal'">
-                          <template v-slot:activator="{ props2 }">
-                            <v-btn variant="flat" icon="mdi-close-box-outline" v-bind="(props1, props2)" @click="clearPersonalBlacklistDialog = true"></v-btn>
-                          </template>
-                        </v-tooltip>
+                    <v-dialog v-if="blacklist.name === 'Personal' && personalBlacklist.length !== 0" v-model="editPersonalBlacklistDialog">
+                      <template v-slot:activator="{ props }">
+                        <v-btn variant="flat" icon="mdi-pencil" v-bind="props" @click="editPersonalBlacklistDialog = true"></v-btn>
                       </template>
-                      <v-card>
-                        <v-card-text>You are about to clear your entire personal blacklist. This can take up to a minute to take affect. Please confirm below.</v-card-text>
+                      <v-card title="Manage Reports">
+                        <div class="ml-4 mr-4 mt-4 mb-4">
+                          <v-text-field v-model="personalBlacklistSearch" class="ml-2" variant="outlined" density="compact" single-line hide-details prepend-inner-icon="mdi-account-search" persistent-placeholder placeholder="Search player(s)"></v-text-field>
+                          <v-data-table show-select item-value="UUID" v-model="personalBlacklistSelection" :loading="loadingPersonalBlacklist" :search="personalBlacklistSearch" :headers="personalBlacklistHeaders" :items="personalBlacklist" itemsPerPageText="Reports per page" itemsPerPage="10"> </v-data-table>
+                        </div>
                         <v-card-actions class="justify-center">
-                          <v-btn color="success" variant="outlined" @click="clearPersonalBlacklist">Confirm</v-btn>
-                          <v-btn color="error" variant="outlined" @click="clearPersonalBlacklistDialog = false">Cancel</v-btn>
+                          <v-btn prepend-icon="mdi-delete" color="warning" variant="outlined" @click="clearPersonalBlacklistSelection">Delete Selection</v-btn>
+                          <v-btn prepend-icon="mdi-cancel" color="error" variant="outlined" @click="editPersonalBlacklistDialog = false">Cancel</v-btn>
                         </v-card-actions>
                       </v-card>
                     </v-dialog>
@@ -62,7 +61,13 @@
 import dataStore from "../../data/dataStore";
 import { ref } from "vue";
 import axios from "axios";
+import moment from "moment";
 import { sendNotification } from "../../misc/snackbarNotification";
+
+moment.relativeTimeThreshold("M", 12);
+moment.relativeTimeThreshold("w", 4);
+moment.relativeTimeThreshold("d", 7);
+moment.relativeTimeThreshold("h", 24);
 
 const blacklists = ref(0);
 blacklists.value = dataStore.get("blacklists");
@@ -71,34 +76,80 @@ const toggleList = (name) => {
   dataStore.set("blacklists", blacklists.value);
 };
 
-const clearPersonalBlacklistDialog = ref(0);
-clearPersonalBlacklistDialog.value = false;
+const personalBlacklist = ref(0);
+const personalBlacklistSearch = ref(0);
+personalBlacklistSearch.value = "";
 
-const clearPersonalBlacklist = () => {
-  clearPersonalBlacklistDialog.value = false;
+const editPersonalBlacklistDialog = ref(0);
+editPersonalBlacklistDialog.value = false;
+
+const loadingPersonalBlacklist = ref(0);
+loadingPersonalBlacklist.value = true;
+
+const updatePersonalBlacklist = () => {
+  loadingPersonalBlacklist.value = true;
   axios
-    .delete("https://api.pixelic.de/hypixel/v1/overlay/reportsystem/list/personal", {
-      headers: {
-        "x-api-key": dataStore.get("pixelicKey"),
-      },
-      timeout: 10000,
+    .get("https://api.pixelic.de/hypixel/v1/overlay/reportsystem/list/personal", { params: { extended: "true" }, headers: { "X-API-Key": dataStore.get("pixelicKey"), "cache-control": "no-cache" } })
+    .then(async (res) => {
+      const rawList = res.data.reports;
+      const parsedList = [];
+      const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+      for (const report of rawList) {
+        parsedList.push({ ...report, reason: capitalize(report.reason), timestampExpiry: moment(report.timestampExpiry * 1000).fromNow() });
+      }
+      personalBlacklist.value = parsedList;
+      loadingPersonalBlacklist.value = false;
     })
-    .then(() =>
-      sendNotification({
-        timeout: 5000,
-        color: "success",
-        icon: "mdi-database-sync-outline",
-        text: "Your personal blacklist was cleared successfully.",
+    .catch(() => {
+      personalBlacklist.value = [];
+      loadingPersonalBlacklist.value = false;
+    });
+};
+updatePersonalBlacklist();
+
+const personalBlacklistHeaders = [
+  { title: "Username", align: "center", key: "username" },
+  { title: "UUID", align: "center", key: "UUID" },
+  { title: "Expiring", align: "center", key: "timestampExpiry" },
+  { title: "Reason", align: "center", key: "reason" },
+];
+
+var personalBlacklistSelection = ref(0);
+personalBlacklistSelection.value = [];
+
+const clearPersonalBlacklistSelection = () => {
+  editPersonalBlacklistDialog.value = false;
+
+  if (personalBlacklistSelection.value.length !== 0) {
+    axios
+      .delete("https://api.pixelic.de/hypixel/v1/overlay/reportsystem/reports", {
+        params: {
+          UUIDs: personalBlacklistSelection.value.join(","),
+        },
+        headers: {
+          "X-API-Key": dataStore.get("pixelicKey"),
+        },
+        timeout: 10000,
       })
-    )
-    .catch(() =>
-      sendNotification({
-        timeout: 5000,
-        color: "error",
-        icon: "mdi-database-sync-outline",
-        text: "An error occured whilst trying to clear your personal blacklist.",
+      .then(() => {
+        sendNotification({
+          timeout: 5000,
+          color: "success",
+          icon: "mdi-database-sync-outline",
+          text: "Your selected reports from your personal blacklist were deleted successfully.",
+        });
+        updatePersonalBlacklist();
       })
-    );
+      .catch(() =>
+        sendNotification({
+          timeout: 5000,
+          color: "error",
+          icon: "mdi-database-sync-outline",
+          text: "An error occured whilst trying to delete your selected reports from your personal blacklist.",
+        })
+      );
+    personalBlacklistSelection.value = [];
+  }
 };
 
 const cheaterExpireOptions = ["7d", "30d", "90d", "180d", "1y", "3y", "5y", "10y"];
